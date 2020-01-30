25413fe7ef5e59fe7d2445d624cb663caba12149
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-30 15:42:38 +0200

    added FormsModule to modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
@@ -26,7 +27,8 @@ import { CurrencyListItemComponent } from './components/currency-list-item/curre
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AppBootstrapModule
+    AppBootstrapModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

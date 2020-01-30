135c9f3274c5a883d054af072a4ce8fc95ac1e66
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-30 11:19:45 +0200

    connected new components and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductListItemComponent } from './components/product-list-item/product-list-item.component';
+import { HeadingComponent } from './components/heading/heading.component';
+import { CurrencyListComponent } from './components/currency-list/currency-list.component';
+import { CurrencyListItemComponent } from './components/currency-list-item/currency-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductListItemComponent
+    ProductListItemComponent,
+    HeadingComponent,
+    CurrencyListComponent,
+    CurrencyListItemComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AppBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]

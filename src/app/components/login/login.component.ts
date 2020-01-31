557550f3844b7dce12106560db790b6ced658c05
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-31 16:03:28 +0200

    used LoginData class for validation rules

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -21,8 +21,8 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
-      password: ['', [Validators.required, Validators.minLength(8)]]
+      email: ['', LoginData.rules.email],
+      password: ['', LoginData.rules.password]
     });
   }
 

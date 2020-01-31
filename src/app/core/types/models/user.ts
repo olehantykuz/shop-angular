891fffad8b427ae4c2ca939b25a777cba8026933
Oleh Antykuz <ovantykuz@gmail.com>
2020-01-31 15:53:47 +0200

    created class RegisterData, moved validation rules to LoginData and RegisteData classes

--- src/app/core/types/models/user.ts
+++ src/app/core/types/models/user.ts
@@ -1,6 +1,19 @@
+import { Validators } from '@angular/forms';
+
 export class LoginData {
+  static rules = {
+    email: [Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')],
+    password: [Validators.required, Validators.minLength(8)]
+  };
+
   email: string;
   password: string;
+}
+
+export class RegisterData extends LoginData {
+  static rules = Object.assign({}, LoginData.rules, {name: [Validators.required]});
+
+  name: string;
 }
 
 export class User {
@@ -8,5 +21,3 @@ export class User {
   email: string;
   name: string | null;
 }
-
-

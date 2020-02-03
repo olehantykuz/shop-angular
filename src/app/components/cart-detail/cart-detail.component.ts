54441dbf67691d9008e1f4ab3f70aa7ab10595bb
Oleh Antykuz <ovantykuz@gmail.com>
2020-02-03 14:10:39 +0200

    added cart-detail component; registered cart-detail and cart-detail-items components

---
+++ src/app/components/cart-detail/cart-detail.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { CurrencyService } from '../../services/currency.service';
+import { CartService } from '../../services/cart.service';
+import { CartItem } from '../../core/types/models/cart';
+
+@Component({
+  selector: 'app-cart-detail',
+  templateUrl: './cart-detail.component.html',
+  styleUrls: ['./cart-detail.component.scss']
+})
+export class CartDetailComponent implements OnInit {
+  items: CartItem[] = this.cartService.items;
+
+  constructor(
+    private currencyService: CurrencyService,
+    private cartService: CartService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  selectedCurrencyName() {
+    return this.currencyService.selectedCurrency;
+  }
+
+  getItems() {
+    return this.items = this.cartService.items;
+  }
+
+  getTotalCost() {
+    const totalCostInBaseCurrency = this.cartService.calculateTotalCost();
+
+    return (this.currencyService.getConvertedValue(totalCostInBaseCurrency) / 100).toFixed(2);
+  }
+
+}

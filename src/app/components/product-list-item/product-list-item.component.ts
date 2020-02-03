5032ff7562e4eb1e0ea395dcf072e89bc105b05e
Oleh Antykuz <ovantykuz@gmail.com>
2020-02-03 11:45:20 +0200

    added ability of adding product to cart

--- src/app/components/product-list-item/product-list-item.component.ts
+++ src/app/components/product-list-item/product-list-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../../core/types/models/product';
 import { CurrencyService } from '../../services/currency.service';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: '[app-product-list-item]',
@@ -9,9 +10,11 @@ import { CurrencyService } from '../../services/currency.service';
 })
 export class ProductListItemComponent implements OnInit {
   @Input() product: Product;
+  quantity = 1;
 
   constructor(
-    private currencyService: CurrencyService
+    private currencyService: CurrencyService,
+    private cartService: CartService
   ) { }
 
   ngOnInit() {
@@ -20,6 +23,10 @@ export class ProductListItemComponent implements OnInit {
   getFormattedPrice() {
     return (this.currencyService.getConvertedValue(this.product.price))
       .toFixed(2);
+  }
+
+  addToCart() {
+    this.cartService.addItem(this.product, +this.quantity);
   }
 
 }
